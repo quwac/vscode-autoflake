6d4d68c73a0d726d259c303f5e6f3d793dc421d2
quwac <53551867+quwac@users.noreply.github.com>
2022-01-24 19:00:53 +0900

    Add variable interpretation in paths: ${workspaceFolder}, ${env:FOO}

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,64 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
+const WORKSPACE_FOLDER = '${workspaceFolder}';
+const ENV_VAR_REGEX = new RegExp('\\$\\{env:([^}]+)\\}');
+
+// const logger = vscode.window.createOutputChannel('vscode-autoflake');
+
+function resolveWorkspaceFolder(filePath: string): string {
+  if (filePath.indexOf(WORKSPACE_FOLDER) == -1) {
+    return filePath;
+  }
+
+  const uri = vscode.window.activeTextEditor?.document?.uri;
+  if (uri) {
+    let workspaceFolderPath = vscode.workspace.getWorkspaceFolder(uri);
+    if (workspaceFolderPath === undefined) {
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (workspaceFolders?.length) {
+        workspaceFolderPath = workspaceFolders[0];
+      }
+    }
+    if (workspaceFolderPath) {
+      return filePath.replace(WORKSPACE_FOLDER, workspaceFolderPath.uri.fsPath);
+    }
+  }
+  return filePath;
+}
+
+function resolveEnvVar(text: string): string {
+  const envVarNames = [];
+
+  let result = '';
+  let tmp = text;
+  while (tmp != '') {
+    const m = ENV_VAR_REGEX.exec(tmp);
+    if (m) {
+      const envVarName = m[1];
+      envVarNames.push(envVarName);
+
+      const matchedString = m[0];
+      const startIndex = tmp.indexOf(matchedString);
+      const endIndex = startIndex + matchedString.length;
+      const envVarValue = process.env[envVarName];
+      result += tmp.substring(0, startIndex) + envVarValue;
+      tmp = tmp.substring(endIndex);
+    } else {
+      result += tmp;
+      break;
+    }
+  }
+
+  return result;
+}
+
+function resolvePath(filePath: string): string {
+  filePath = resolveWorkspaceFolder(filePath);
+  filePath = resolveEnvVar(filePath);
+  return filePath;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,7 +68,7 @@ export function activate(context: vscode.ExtensionContext) {
   const remove_unused = (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
     // python config
     let pythonConfig = vscode.workspace.getConfiguration('python');
-    const isortPath = pythonConfig.get('sortImports.path', '');
+    const isortPath = resolvePath(pythonConfig.get('sortImports.path', ''));
     const isortArgs = pythonConfig.get('sortImports.args', []);
     // autoflake config
     let config = vscode.workspace.getConfiguration('autoflake');
@@ -19,7 +77,7 @@ export function activate(context: vscode.ExtensionContext) {
     const all_imports = config.get('removeAllUnusedImports');
     const remove_vars = config.get('removeAllUnusedVariables');
     const remove_duplicate = config.get('removeDuplicateKeys');
-    const autoflake = config.get('path');
+    const autoflake = resolvePath(config.get('path', ''));
     // get the activate editor
     const filepath = textEditor.document.uri.fsPath;
     // skip if not python file
