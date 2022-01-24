8f8dca6d29ad8574e3d69bc984f578ab9a750309
quwac <53551867+quwac@users.noreply.github.com>
2022-01-24 22:29:37 +0900

    Add virtualenv path traversal by Poetry

--- src/extension.ts
+++ src/extension.ts
@@ -2,29 +2,40 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as process from 'process';
 
 const WORKSPACE_FOLDER = '${workspaceFolder}';
 const ENV_VAR_REGEX = new RegExp('\\$\\{env:([^}]+)\\}');
+const ACTIVATED_PHRASE = ' (Activated)';
 
 // const logger = vscode.window.createOutputChannel('vscode-autoflake');
+
+function getWorkspaceFolderPath(): string | null {
+  const uri = vscode.window.activeTextEditor?.document?.uri;
+  let workspaceFolderPath: string | null = null;
+  if (uri) {
+    let workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
+    if (workspaceFolderPath === null) {
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (workspaceFolders?.length) {
+        workspaceFolder = workspaceFolders[0];
+      }
+    }
+    workspaceFolderPath = workspaceFolder?.uri?.fsPath || null;
+  }
+  return workspaceFolderPath;
+}
 
 function resolveWorkspaceFolder(filePath: string): string {
   if (filePath.indexOf(WORKSPACE_FOLDER) == -1) {
     return filePath;
   }
 
-  const uri = vscode.window.activeTextEditor?.document?.uri;
-  if (uri) {
-    let workspaceFolderPath = vscode.workspace.getWorkspaceFolder(uri);
-    if (workspaceFolderPath === undefined) {
-      const workspaceFolders = vscode.workspace.workspaceFolders;
-      if (workspaceFolders?.length) {
-        workspaceFolderPath = workspaceFolders[0];
-      }
-    }
-    if (workspaceFolderPath) {
-      return filePath.replace(WORKSPACE_FOLDER, workspaceFolderPath.uri.fsPath);
-    }
+  const workspaceFolderPath = getWorkspaceFolderPath();
+  if (workspaceFolderPath) {
+    return filePath.replace(WORKSPACE_FOLDER, workspaceFolderPath);
   }
   return filePath;
 }
@@ -61,6 +72,101 @@ function resolvePath(filePath: string): string {
   return filePath;
 }
 
+function getPoetryPath(workspaceFolderPath: string): string {
+  const pythonSection = vscode.workspace.getConfiguration('python');
+  let poetryPathCandidate: string | undefined | null = null;
+  if (pythonSection) {
+    poetryPathCandidate = pythonSection.get('poetry');
+    if (poetryPathCandidate) {
+      poetryPathCandidate = resolveWorkspaceFolder(poetryPathCandidate);
+      poetryPathCandidate = resolveEnvVar(poetryPathCandidate);
+    }
+  }
+  poetryPathCandidate = poetryPathCandidate || 'poetry';
+
+  if (fs.existsSync(poetryPathCandidate)) {
+    return poetryPathCandidate;
+  }
+
+  const inCwd = path.join(process.cwd(), poetryPathCandidate);
+  if (fs.existsSync(inCwd)) {
+    return inCwd;
+  }
+
+  const inWorkspace = workspaceFolderPath
+    ? path.join(workspaceFolderPath, poetryPathCandidate)
+    : null;
+  if (inWorkspace && fs.existsSync(inWorkspace)) {
+    return inWorkspace;
+  }
+
+  const envPathText = process.env['PATH'];
+  const delimiter = process.platform == 'win32' ? ';' : ':';
+  const poetryRelativePath = poetryPathCandidate;
+  const poetryPath = envPathText
+    ?.split(delimiter)
+    .map((envPath) => path.join(envPath, poetryRelativePath))
+    ?.find((poetryPath) => fs.existsSync(poetryPath));
+
+  return poetryPath || poetryPathCandidate;
+}
+
+function getPoetryVirtualenvPath(workspaceFolderPath: string): string | null {
+  const poetryPath = getPoetryPath(workspaceFolderPath);
+  const command = [
+    'cd',
+    workspaceFolderPath,
+    '&&',
+    poetryPath,
+    'env',
+    'list',
+    '--full-path',
+  ].join(' ');
+  const result = child_process.execSync(command).toString();
+
+  const candidate = result
+    .split('\n')
+    .find((line) => line.indexOf(ACTIVATED_PHRASE) > 0)
+    ?.replace('\r', '')
+    ?.replace(ACTIVATED_PHRASE, '');
+
+  return candidate && fs.existsSync(candidate) ? candidate : null;
+}
+
+function getVirtualenvBinPath(): string | null {
+  const workspaceFolderPath = getWorkspaceFolderPath();
+  if (!workspaceFolderPath) {
+    return null;
+  }
+
+  const pyprojectTomlPath = path.join(workspaceFolderPath, 'pyproject.toml');
+  if (fs.existsSync(pyprojectTomlPath)) {
+    const virtualenvPath = getPoetryVirtualenvPath(workspaceFolderPath);
+    return virtualenvPath ? path.join(virtualenvPath, 'bin') : null;
+  }
+
+  return null;
+}
+
+function resolveAutoflakePath(filePath: string): string {
+  let resolvedPath = resolvePath(filePath);
+
+  if (fs.existsSync(resolvedPath)) {
+    return resolvedPath;
+  }
+
+  const virtualenvBinPath = getVirtualenvBinPath();
+  if (virtualenvBinPath) {
+    resolvedPath = path.join(virtualenvBinPath, filePath);
+
+    if (fs.existsSync(resolvedPath)) {
+      return resolvedPath;
+    }
+  }
+
+  return filePath;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -77,7 +183,7 @@ export function activate(context: vscode.ExtensionContext) {
     const all_imports = config.get('removeAllUnusedImports');
     const remove_vars = config.get('removeAllUnusedVariables');
     const remove_duplicate = config.get('removeDuplicateKeys');
-    const autoflake = resolvePath(config.get('path', ''));
+    const autoflake = resolveAutoflakePath(config.get('path', ''));
     // get the activate editor
     const filepath = textEditor.document.uri.fsPath;
     // skip if not python file
